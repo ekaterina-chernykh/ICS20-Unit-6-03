b2324bf8dbb602212ec6dee3c8f4547a26175941
ekaterina-chernykh <ekaterina-chernykh@users.noreply.github.com>
2023-01-06 04:13:05 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ const weather = async (URLAddress) => {
       "<img src=' https://openweathermap.org/img/wn/10d@2x.png" +
       jsonData.weather[0] +
       "@2x.png alt='image'width=‘auto’ height=‘auto’" +
-      (">")
+      ">"
 
     document.getElementById("temp").innerHTML =
       "The tempurature outside is " + temp2.toFixed(2) + "° C"
